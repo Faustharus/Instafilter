ff2438e94b2f4982ec488e3f3ab8dc16199b6f85
Faustharus <chailloleau-damien@outlook.fr>
2024-06-24 10:57:39 +0200

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Damien Chailloleau on 24/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
